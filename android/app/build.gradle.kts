533252f7d5151a34ca02d5756750f3de2a381d2b
MIDO12A <MIDO12A@users.noreply.github.com>
2026-08-30 06:27:41 +0300

    fix: disable R8 minifyEnabled to avoid plugin stripping in release APK, bump to 1.0.41+2000003500

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -53,12 +53,8 @@ android {
                 signingConfigs.getByName("release")
             else
                 signingConfigs.getByName("debug")
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
